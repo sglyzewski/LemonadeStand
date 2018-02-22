bf8a8467fa1a913554dfd6d705561961a13ce726
Stephanie Glyzewski <stephanie.glyzewski@gmail.com>
2018-02-22 15:48:04 -0600

    classes created

---
+++ Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Player
+    {
+        double profit;
+        double expenses;
+        double income;
+        double liquidatedInventoryValue; //total worth of a company's physical assets when it goes out of business; determined by assets i.e. inventory
+
+    }
+}
